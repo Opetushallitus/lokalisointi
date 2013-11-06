def530f17a97bc6178cfe02d3c62306b85511f88
Marko Lyly <Marko.Lyly@proactum.fi>
2013-11-06 10:43:06 +0200

    OVT-6157 Verson to 8.0-SNAPSHOT, service url to be by defautl "/lokalisointi-service" - should work in "prod" envs.

--- lokalisointi-app/src/main/webapp/ext/js/env-configuration.js
+++ lokalisointi-app/src/main/webapp/ext/js/env-configuration.js
@@ -3,7 +3,9 @@ window.CONFIG = window.CONFIG || {};
 window.CONFIG.env = window.CONFIG.env || {};
 window.CONFIG.env.localisations = window.CONFIG.env.localisations || [];
 
-window.CONFIG.env.casUrl = "cas_myroles.json";
+// window.CONFIG.env.casUrl = "cas_myroles.json";
+window.CONFIG.env.casUrl = "/cas/myroles";
 
-// window.CONFIG.env.lokalisointiRestUrl = "http://luokka.hard.ware.fi:8319/lokalisointi-service/rest/v1/localisation";
-window.CONFIG.env.lokalisointiRestUrl = "http://localhost:8319/lokalisointi-service/rest/v1/localisation";
+// window.CONFIG.env.lokalisointiRestUrl = "https://itest-virkailija.oph.ware.fi/lokalisointi-service/rest/v1/localisation";
+// window.CONFIG.env.lokalisointiRestUrl = "http://localhost:8319/lokalisointi-service/rest/v1/localisation";
+window.CONFIG.env.lokalisointiRestUrl = "/lokalisointi-service/rest/v1/localisation";
