e86d42a9aaa06762ff042703c796d1baa0c923ff
Marko Lyly <Marko.Lyly@proactum.fi>
2013-11-06 10:45:28 +0200

    OVT-6157 config confusion

--- lokalisointi-app/src/main/webapp/ext/js/env-configuration.js
+++ lokalisointi-app/src/main/webapp/ext/js/env-configuration.js
@@ -6,6 +6,11 @@ window.CONFIG.env.localisations = window.CONFIG.env.localisations || [];
 // window.CONFIG.env.casUrl = "cas_myroles.json";
 window.CONFIG.env.casUrl = "/cas/myroles";
 
-// window.CONFIG.env.lokalisointiRestUrl = "https://itest-virkailija.oph.ware.fi/lokalisointi-service/rest/v1/localisation";
+// Localhost service testing
 // window.CONFIG.env.lokalisointiRestUrl = "http://localhost:8319/lokalisointi-service/rest/v1/localisation";
+
+// Luokka service testing
+// window.CONFIG.env.lokalisointiRestUrl = "https://itest-virkailija.oph.ware.fi/lokalisointi-service/rest/v1/localisation";
+
+// Production
 window.CONFIG.env.lokalisointiRestUrl = "/lokalisointi-service/rest/v1/localisation";
