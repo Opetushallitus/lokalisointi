a62d9e1e201c90c491a671cfaba79dccb8c191d6
Marko Lyly <Marko.Lyly@proactum.fi>
2013-11-07 16:25:11 +0200

    OVT-6157 Added healthcheck.

--- lokalisointi-service/src/main/java/fi/vm/sade/lokalisointi/service/HealthCheck.java
+++ lokalisointi-service/src/main/java/fi/vm/sade/lokalisointi/service/HealthCheck.java
@@ -12,12 +12,9 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  */
-
 package fi.vm.sade.lokalisointi.service;
 
 import fi.vm.sade.generic.healthcheck.SpringAwareHealthCheckServlet;
-
-
 
 /**
  *
