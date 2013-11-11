afde17927503cdd00b306c1a93da0ec3ed61226f
Marko Lyly <Marko.Lyly@proactum.fi>
2013-11-11 13:57:20 +0200

    OVT-6157 lokalisointi re-organisation to single app

--- lokalisointi/src/main/webapp/ext/js/env-configuration.js
+++ lokalisointi/src/main/webapp/ext/js/env-configuration.js
@@ -7,10 +7,10 @@ window.CONFIG.env.localisations = window.CONFIG.env.localisations || [];
 window.CONFIG.env.casUrl = "/cas/myroles";
 
 // Localhost service testing
-// window.CONFIG.env.lokalisointiRestUrl = "http://localhost:8319/lokalisointi-service/rest/v1/localisation";
+// window.CONFIG.env.lokalisointiRestUrl = "http://localhost:8319/lokalisointi/cxf/rest/v1/localisation";
 
 // Luokka service testing
-// window.CONFIG.env.lokalisointiRestUrl = "https://itest-virkailija.oph.ware.fi/lokalisointi-service/rest/v1/localisation";
+// window.CONFIG.env.lokalisointiRestUrl = "https://itest-virkailija.oph.ware.fi/lokalisointi/cxf/rest/v1/localisation";
 
 // Production
-window.CONFIG.env.lokalisointiRestUrl = "/lokalisointi-service/cxf/rest/v1/localisation";
+window.CONFIG.env.lokalisointiRestUrl = "/lokalisointi/cxf/rest/v1/localisation";
