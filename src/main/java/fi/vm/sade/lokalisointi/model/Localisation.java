49e6a6ef56a13132cb69fe13db8b46552f4ae290
Tomi Katajanheimo <tomi@katajanheimo.com>
2024-12-11 13:18:21 +0200

    Palauta myös null-arvoiset kentät

--- src/main/java/fi/vm/sade/lokalisointi/model/Localisation.java
+++ src/main/java/fi/vm/sade/lokalisointi/model/Localisation.java
@@ -1,13 +1,11 @@
 package fi.vm.sade.lokalisointi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 public class Localisation {
